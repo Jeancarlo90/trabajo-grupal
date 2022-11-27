5ff0cd8f86a3e454cd21b05d440f58df490b5076
Jeancarlo90 <jeancarloac60@gmail.com>
2022-11-27 12:51:58 -0500

    historia12-parte4

--- src/Components/TabsComponents.js
+++ src/Components/TabsComponents.js
@@ -5,21 +5,38 @@ import  '../App.css'
 const TabsComponents = (props) => {
     return(
         <main>
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+            <ul class="nav nav-pills mb-3 nav-fill" id="pills-tab" role="tablist">
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link active"  id="pills-graphics-tab" data-bs-toggle="pill" data-bs-target="#pills-graphics" type="button" role="tab" aria-controls="pills-graphics" aria-selected="true">Graphics</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link" id="pills-processor-tab" data-bs-toggle="pill" data-bs-target="#pills-processor" type="button" role="tab" aria-controls="pills-processor" aria-selected="false">Processor</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link" id="pills-memory-tab" data-bs-toggle="pill" data-bs-target="#pills-memory" type="button" role="tab" aria-controls="pills-memory" aria-selected="false">Memory</button>
                 </li>
-                </ul>
+
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link" id="pills-storage-tab" data-bs-toggle="pill" data-bs-target="#pills-storage" type="button" role="tab" aria-controls="pills-storage" aria-selected="false">Storage</button>
+                </li>
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link" id="pills-cooler-tab" data-bs-toggle="pill" data-bs-target="#pills-cooler" type="button" role="tab" aria-controls="pills-cooler" aria-selected="false">Cooler</button>
+                </li>
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link" id="pills-windows-tab" data-bs-toggle="pill" data-bs-target="#pills-windows" type="button" role="tab" aria-controls="pills-windows" aria-selected="false">Windows</button>
+                </li>
+                <li class="nav-item me-2" role="presentation">
+                    <button class="nav-link" id="pills-power-tab" data-bs-toggle="pill" data-bs-target="#pills-power" type="button" role="tab" aria-controls="pills-power" aria-selected="false">Power Supply</button>
+                </li>
+            </ul>
                 <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">TAB UNO</div>
-                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab"><p className='text-white'> HOLA</p></div>
-                <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
+                <div class="tab-pane fade show active" id="pills-graphics" role="tabpanel" aria-labelledby="pills-graphics-tab">TAB UNO</div>
+                <div class="tab-pane fade" id="pills-processor" role="tabpanel" aria-labelledby="pills-processor-tab"><p className='text-white'> HOLA</p></div>
+                <div class="tab-pane fade" id="pills-memory" role="tabpanel" aria-labelledby="pills-memory-tab">...</div>
+                <div class="tab-pane fade" id="pills-storage" role="tabpanel" aria-labelledby="pills-storage-tab">...</div>
+                <div class="tab-pane fade" id="pills-cooler" role="tabpanel" aria-labelledby="pills-cooler-tab">...</div>
+                <div class="tab-pane fade" id="pills-windows" role="tabpanel" aria-labelledby="pills-windows-tab">...</div>
+                <div class="tab-pane fade" id="pills-power" role="tabpanel" aria-labelledby="pills-power-tab">...</div>
             </div>
         </main>
     )
