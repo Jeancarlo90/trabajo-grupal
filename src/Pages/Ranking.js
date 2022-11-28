0344f74f6969c0fc04b55047a7c25bdeff6fb08f
Jeancarlo90 <jeancarloac60@gmail.com>
2022-11-27 20:36:29 -0500

    semcompleted

--- src/Pages/Ranking.js
+++ src/Pages/Ranking.js
@@ -1,14 +1,32 @@
 import  '../App.css'
-import dataRanking from '../Data/DataRanking.json'
+import React, { useEffect, useState } from "react"
 import TarjetaRanking from '../Components/TarjetaRanking';
+
+const BASE_URL = "https://backendtrabajo-production.up.railway.app"
+
 function Ranking(){
+    const [componentsList, setComponentsList] = useState([])
+
+    const httpGetComponentssAsyncAwait = async () => {
+        const resp = await fetch(
+            `${BASE_URL}/components/bestseller`
+        )
+        const data = await resp.json()
+        setComponentsList(data)
+    }
+
+    useEffect(() => {
+        httpGetComponentssAsyncAwait()
+    }, [])
+
+
     return(
         <main className='bg-dark h-100'>
             <div className='container'>
                 <h3 className='text-light'> Ranking Best Sellers Periferics</h3>
                 <div className='row mt-5 '>
             {
-                    dataRanking.map(item => (
+                    componentsList.map(item => (
                         <TarjetaRanking  key={item.id} periferico={item}></TarjetaRanking>
                     ))
                 }
