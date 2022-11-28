31573b7def5b922b6e8780d29d1b6eccf2e9f7ae
Jeancarlo90 <jeancarloac60@gmail.com>
2022-11-27 20:20:55 -0500

    conect

--- src/Components/UserReviews.js
+++ src/Components/UserReviews.js
@@ -1,17 +1,43 @@
 import  '../App.css'
+import React, { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
 import { BsFillStarFill } from 'react-icons/bs';
-import dataReviews from '../Data/DataReviews.json';
 
-function getStarts() {
-    let content = [];
-    for (var i=0; i < dataReviews.start; i++) {
-      content.push(<BsFillStarFill key={i} style={{color:"#ffc107"}}/>);
-    }
-    return content;
-  };
 
+const BASE_URL = "https://backendtrabajo-production.up.railway.app"
 
 function Reviews(){
+
+    const [reviewList, setReviewList] = useState([])
+    const [stars, setStarts] = useState(0)
+
+
+    const httpGetReviewAsyncAwait = async () => {
+        const resp = await fetch(
+            `${BASE_URL}/reviews`
+        )
+        const data = await resp.json()
+        setReviewList(data)
+        const arr = [];
+
+        data.forEach(item => {
+            arr.push(item.stars)
+        })
+        setStarts(arr.reduce((a,b) => a + b)/arr.length)
+    }
+    useEffect(() => {
+        httpGetReviewAsyncAwait()
+    }, [])
+
+    function getStarts() {
+        let content = [];
+        for (var i=0; i < stars; i++) {
+          content.push(<BsFillStarFill key={i} style={{color:"#ffc107"}}/>);
+        }
+        return content;
+      };
+
     return(
         <main className='container'>
             <h3> User Reviews</h3>
@@ -24,7 +50,7 @@ function Reviews(){
                 </div>
             </div>
              {
-                dataReviews.reviews.map(item => (
+                reviewList.map(item => (
                     <div key={item.id} className='row'>
                         <><hr />
                         <div>
